23054643e36077ef893699853124110ef4482fb7
unknown <rislammb@gmail.com>
2022-06-16 13:09:02 +0600

    try to lodash

--- practice/21-lodash.js
+++ practice/21-lodash.js
@@ -72,20 +72,59 @@ const difference = (arr, values) => {
 // const result = difference(array, [2, 3, 9, 4, 2]);
 // console.log(result);
 
-// ##### 5. _.difference function
+// ##### 5. _.drop function
 const drop = (arr, n=1) => {
   if (arr && arr.length > 0) {
-      return arr.reduce((acc, cur, index) => {
-        if (index > n - 1) {
-          acc.push(cur);
-        }
-        return acc;
-      }, [])
-  } else {
-    return [];
-  }
-}
-const array = [1, 2, 5, 9, 3];
-const result = drop(array, 3);
-console.log(result);
+      // method 1
+      const newArr = [...arr];
+      for (let i = 0; i < n; i++) {
+        newArr.shift();
+      }
+      return newArr;
 
+      // // method 2
+      // return arr.reduce((acc, cur, index) => {
+      //   if (index > n - 1) {
+      //     acc.push(cur);
+      //   }
+      //   return acc;
+      // }, []);
+  } else return []; 
+}
+// const array = [1, 2, 5, 8, 9, 3];
+// const result = drop(array, 3);
+// console.log(result);
+
+// ##### 6. _.dropRight function
+const dropRight = (arr, n=1) => {
+  if (arr && arr.length > 0) {
+    // method 1
+    const newArr = [...arr];
+    for (let i = 0; i < n; i++) {
+      newArr.pop();
+    }
+    return newArr;
+
+    // // method 2
+    // return arr.reduce((acc, cur, index) => {
+    //   if (index < arr.length - n) {
+    //     acc.push(cur);
+    //   }
+    //   return acc;
+    // }, []);
+  } else return []; 
+}
+// const array = [1, 2, 5, 8, 9, 3];
+// const result = dropRight(array, 2);
+// console.log(result);
+
+// ##### 7. _.fill function
+const fill = (arr, val, start = 0, end = arr.length) => {
+  for (let i = start; i < end; i++) {
+    arr[i] = val;
+  }
+  return arr;
+}
+const array = [4, 6, 8, 10, 7];
+const result = fill(array, '*', 1, 3);
+console.log(result);
