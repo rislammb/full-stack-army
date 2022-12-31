cc77f5a25a6e3470345088f6f262a2573f6e7ed3
unknown <rislammb@gmail.com>
2022-12-31 14:30:13 +0600

    add viewRoutes.js for separate api and ejs view app

---
+++ projects/raffle-draw/viewRoutes.js
@@ -0,0 +1,31 @@
+const router = require('express').Router();
+const {
+  sellSingleTicket,
+  sellBulkTicket,
+  findAll,
+  findById,
+  findByUsername,
+  updateById,
+  updateByUsername,
+  deleteById,
+  deleteByUsername,
+  drawWinners,
+} = require('./controllers');
+
+// router.get('/t/:id');
+// router.put('/t/:id');
+// router.delete('/t/:id');
+
+router.route('/t/:id').get(findById).put(updateById).delete(deleteById);
+
+router
+  .route('/u/:username')
+  .get(findByUsername)
+  .put(updateByUsername)
+  .delete(deleteByUsername);
+
+router.post('/bulk', sellBulkTicket);
+router.get('/draw', drawWinners);
+router.route('/').get(findAll).post(sellSingleTicket);
+
+module.exports = router;
