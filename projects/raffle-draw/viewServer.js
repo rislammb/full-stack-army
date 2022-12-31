a319ab8b713b4a5f5795079dd5c858ea15bce5c5
unknown <rislammb@gmail.com>
2022-12-31 14:28:30 +0600

    add viewServer.js for separate api and ejs view app

---
+++ projects/raffle-draw/viewServer.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+
+const app = express();
+app.set('view engine', 'ejs');
+app.use([morgan('dev'), cors(), express.json(), express.static('views')]);
+
+// app.use('/api/v1/tickets', require('./routes'));
+app.use('/tickets', require('./routes'));
+
+app.get('/health', (_req, res) => {
+  res.status(200).json({ message: 'Success' });
+});
+
+app.get('/', (_req, res) => {
+  res.render('home');
+});
+
+app.use((_req, _res, next) => {
+  const error = new Error('Resource Not Found!');
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, _req, res, _next) => {
+  if (error.status) {
+    return res.status(error.status).json({ message: error.message });
+  } else res.status(500).json({ message: 'Somethings went wrong!' });
+});
+
+const port = process.env.PORT || 4000;
+app.listen(port, () => console.log(`Server is listening on port ${port}`));
