15a1fa5dc22da05e779fd86e6086eec22e9734b6
unknown <rislammb@gmail.com>
2023-01-16 12:19:30 +0600

    update user model and add registration and login system in attendance system server

--- projects/attendance-system/server/models/User.js
+++ projects/attendance-system/server/models/User.js
@@ -1,11 +1,40 @@
 const { model, Schema } = require('mongoose');
 
 const userSchema = new Schema({
-  name: String,
-  email: String,
-  password: String,
-  roles: [String],
-  accountStatus: String
+  name: {
+    type: String,
+    required: true,
+    minLength: 3,
+    maxLength: 15,
+  },
+  email: {
+    type: String,
+    required: true,
+    validate: {
+      validator: function (v) {
+        return /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+          v
+        );
+      },
+      message: (props) => `Invalid email: ${props.value}`,
+    },
+  },
+  password: {
+    type: String,
+    minLength: [6, 'Password is too short'],
+    required: true,
+  },
+  roles: {
+    type: [String],
+    required: true,
+    default: ['STUDENT'],
+  },
+  accountStatus: {
+    type: String,
+    enum: ['PENDING', 'ACTIVE', 'REJECTED'],
+    default: 'PENDING',
+    required: true,
+  },
 });
 
 const User = model('User', userSchema);
--- projects/attendance-system/server/server.js
+++ projects/attendance-system/server/server.js
@@ -7,41 +7,67 @@ const User = require('./models/User');
 const app = express();
 app.use(express.json());
 
-/**
-  hash = hash password
-  user = save name, email and hash to user model
-  return 201
-  end
- */
-app.post('/register', async (req, res) => {
-  const { name, email, password } = req.body;
+app.post('/register', async (req, res, next) => {
+  const { name, email, password, accountStatus } = req.body;
 
   if (!name || !email || !password) {
     return res.status(400).json({ message: 'Invalid data!' });
   }
 
-  let user = await User.findOne({ email });
-  if (user) {
-    return res.status(400).json({ message: 'User already exist!' });
+  try {
+    let user = await User.findOne({ email });
+    if (user) {
+      return res.status(400).json({ message: 'User already exist!' });
+    }
+
+    user = new User({ name, email, password, accountStatus });
+
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(password, salt);
+
+    await user.save();
+
+    res.status(201).json({ message: 'User created successfully', user });
+  } catch (e) {
+    next(e);
   }
-
-  user = new User({ name, email, password });
-
-  const salt = await bcrypt.genSalt(10);
-  user.password = await bcrypt.hash(password, salt);
-
-  await user.save();
-
-  res.status(201).json({ message: 'User created successfully', user });
 });
 
-app.get('/', (_, res) => {
+app.post('/login', async (req, res, next) => {
+  /**
+   * start
+  token = generate token using user info
+  return token
+  end
+   */
+
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: 'Invalid Credentials!' });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: 'Invalid Credentials!' });
+    }
+
+    delete user._doc.password;
+    res.status(200).json({ message: 'Login Successfull', user });
+  } catch (e) {
+    next(e);
+  }
+});
+
+app.get('/', (_req, res) => {
   res.json({ message: 'Thank you' });
 });
 
-app.get('/private', (req, res) => {
-  console.log(req.headers);
-  return res.json({ message: 'private' });
+app.use((err, _req, res, _next) => {
+  console.log(err);
+  res.status(500).json({ message: 'Server error occurred!' });
 });
 
 connectDB('mongodb://localhost:27017/attendace-db')
