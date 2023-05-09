2848f6c3086a293112a4bcd64fa537c5e48f70d4
unknown <rislammb@gmail.com>
2023-05-09 14:40:16 +0600

    start practice on lecture 33

---
+++ practice/frontend-3/README.md
@@ -0,0 +1,1 @@
+# Explore Data Shape
---
+++ practice/frontend-3/src/App.js
@@ -0,0 +1,15 @@
+const socialIcons = [
+  {
+    id: 123,
+    icon: 'fa-facebook-f',
+    color: '#3a589d',
+    tooltip: 'share on facebook',
+    link: 'https://fb.com',
+  },
+];
+
+const App = () => {
+  return <div>App</div>;
+};
+
+export default App;
