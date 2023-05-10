8d1df3a34d6f7057475f712d0cd4c572b44c930a
unknown <rislammb@gmail.com>
2023-05-10 14:25:04 +0600

    practice start on UI to data structure

--- practice/frontend-3/src/App.js
+++ practice/frontend-3/src/App.js
@@ -1,15 +1,48 @@
-const socialIcons = [
-  {
-    id: 123,
-    icon: 'fa-facebook-f',
-    color: '#3a589d',
-    tooltip: 'share on facebook',
-    link: 'https://fb.com',
-  },
-];
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useState } from 'react';
+import { socialIcons } from './data';
+
+library.add(fas, fab, far);
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div style={{ display: 'flex', gap: '4px' }}>
+      {socialIcons.map((item) => (
+        <IconItem item={item} key={item.id} />
+      ))}
+    </div>
+  );
 };
+
+function IconItem({ item }) {
+  const [isHoverd, setIsHovered] = useState(false);
+
+  return (
+    <a
+      href={item.link}
+      style={{
+        color: isHoverd ? '#fff' : '#aaa',
+        backgroundColor: isHoverd ? item.color : '',
+        border: '1px solid #aaa',
+        borderRadius: '50%',
+        width: '28px',
+        height: '28px',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        transition: 'all 0.45s ease',
+      }}
+      title={item.tooltip}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
+      <FontAwesomeIcon icon={item.icon} />
+    </a>
+  );
+}
 
 export default App;
---
+++ practice/frontend-3/src/data.js
@@ -0,0 +1,37 @@
+export const socialIcons = [
+  {
+    id: 123,
+    icon: 'fa-brands fa-facebook-f',
+    color: '#3a589d',
+    tooltip: 'Share on Facebook',
+    link: 'https://facebook.com',
+  },
+  {
+    id: 124,
+    icon: 'fa-brands fa-twitter',
+    color: '#2478ba',
+    tooltip: 'Share on Twitter',
+    link: 'https://twitter.com',
+  },
+  {
+    id: 125,
+    icon: 'fa-regular fa-envelope',
+    color: '#111',
+    tooltip: 'Email to a Friend',
+    link: 'https://gmail.com',
+  },
+  {
+    id: 126,
+    icon: 'fa-brands fa-pinterest-p',
+    color: '#cb2320',
+    tooltip: 'Pin to Pinterest',
+    link: 'https://pinterest.com',
+  },
+  {
+    id: 127,
+    icon: 'fa-brands fa-linkedin-in',
+    color: '#0072b7',
+    tooltip: 'Share on LinkedIn',
+    link: 'https://linkedin.com',
+  },
+];
